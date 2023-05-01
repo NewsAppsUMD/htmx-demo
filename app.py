099842d2b86e529d80092dc9061338cb1e27600a
Derek Willis <dwillis@gmail.com>
2023-05-01 13:30:23 -0400

    testudo

--- app.py
+++ app.py
@@ -2,7 +2,22 @@ from peewee import *
 from flask import Flask, render_template, request, flash, redirect, url_for
 app = Flask(__name__)
 app.secret_key = "3209djno3eu98u3e"
-db = SqliteDatabase('wbb.db')
+db = SqliteDatabase('testudo.db')
 
-class TeamTotal(Model):
-    
+class Course(Model):
+    id = CharField()
+    title = CharField()
+    description = TextField()
+    term = CharField()
+    department = CharField()
+    instructors = CharField()
+    seats = IntegerField()
+
+    class Meta:
+        table_name = "courses"
+        database = db
+
+@app.route('/')
+def index():
+    terms = Course.select().order_by(Course.term.desc())
+    return render_template('index.html', terms=terms)
